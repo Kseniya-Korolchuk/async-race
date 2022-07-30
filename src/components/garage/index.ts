d7b1d2dc1d7e246a3e49dcde6af7ae704c9e5e97
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-07-30 20:22:15 +0300

    feat: create component "garage"

---
+++ src/components/garage/index.ts
@@ -0,0 +1,24 @@
+import { getCars } from "../../services/api";
+import store from "../../services/store";
+import { renderCar } from "../car";
+
+
+export const renderGarage = (): string => `
+<h2>Garage (${store.carsCount} cars)</h2>
+<h3>Page #${store.carsPage}</h3>
+<ul class="main__cars-list">
+  ${store.cars.map(car => `<li>${renderCar(car)}</li>`).join('')}
+</ul>
+`;
+
+export const updateGarage = async (): Promise<void> => {
+  const { items, count } = await getCars(store.carsPage);
+  store.cars = items;
+  store.carsCount = count;
+
+  const NEXT_BTN = document.getElementById('button_next') as HTMLButtonElement;
+  NEXT_BTN.disabled = store.carsPage * 7 >= Number(store.carsCount);
+
+  const PREV_BTN = document.getElementById('button_prev') as HTMLButtonElement;
+  PREV_BTN.disabled = store.carsPage <= 1;
+};
